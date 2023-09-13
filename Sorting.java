bb6bf7c0577766cc3bf00c5ba1b1b6ba48ce50d4
Jack Segil <jacksegil@icloud.com>
2023-09-13 11:34:06 -0700

    Formatting stuff

--- Sorting.java
+++ Sorting.java
@@ -1,9 +1,9 @@
-import java.util.*;
+import java.util.Arrays;
 /**
- * Write a description of class Sorting here.
+ * Contains 4 sorting algorithims.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author Jack Segil
+ * @version 1.0
  */
 public class Sorting
 {
@@ -11,22 +11,27 @@ public class Sorting
     public static long compsM = 0;
     public static long swapsM = 0;
     
+    // Tests whatever sort needs to be tested at the time
     public static void testMethod() {
         int[] test = {9, 8, 4, 5, 6};
+        
         insertionSort(test);
         System.out.println(Arrays.toString(test));
     }
     
+    // Sorts the given array in ascending order using bubble sort
     public static void bubbleSort(int[] input) {
         long comps = 0;
         long swaps = 0;
-        for (int j = 0; j < input.length - 1; j++) {
-            for (int i = 0; i < input.length - 1; i++) {
+        
+        for (int i = 0; i < input.length - 1; i++) {
+            for (int j = 0; j < input.length - 1; j++) {
                 comps++;
-                if (input[i] > input[i+1]) {
-                    int temp = input[i];
-                    input[i] = input[i+1];
-                    input[i+1] = temp;
+                
+                if (input[j] > input[j+1]) {
+                    int temp = input[j];
+                    input[j] = input[j+1];
+                    input[j+1] = temp;
                     swaps++;
                 }
             }
@@ -34,107 +39,116 @@ public class Sorting
         System.out.print(", " + comps + ", " + swaps);
     }
     
+    // Sorts the given array in ascending order using selection sort
     public static void selectionSort(int[] input) {
         long comps = 0;
         long swaps = 0;
-        int start = 0;
-        for (int j = 0; j < input.length; j++) {
-            int min = start;
-            for (int i = start; i < input.length; i++) {
+        
+        for (int i = 0; i < input.length; i++) {
+            int minIndex = i;
+            for (int j = i; j < input.length; j++) {
                 comps++;
-                if (input[i] < input[min]) {
-                    min = i;
+                if (input[j] < input[minIndex]) {
+                    minIndex = j;
                 }
             }
-            //System.out.println(Arrays.toString(input));
-            int temp = input[min];
-            input[min] = input[start];
-            input[start] = temp;
+            int temp = input[minIndex];
+            input[minIndex] = input[i];
+            input[i] = temp;
             swaps++;
-            start++;
         }
         System.out.print(", " + comps + ", " + swaps);
     }
     
+    // Sorts the given array in ascending order using insertion sort
     public static void insertionSort(int[] input) {
         long comps = 0;
         long swaps = 0;
-        for (int i = 1; i < input.length; i++) {
-            int j = i - 1;
-            int temp = input[i];
+        
+        for (int i = 0; i < input.length; i++) {
+            int j = i;
+            int temp = input[j];
             comps++;
-            while (j >= 0 && temp < input[j]) {
+            while (j > 0 && temp < input[j - 1]) {
                 comps++;
-                input[j+1] = input[j];
+                input[j] = input[j - 1];
                 swaps++;
                 j--;
             }
-            input[j+1] = temp;
+            input[j] = temp;
             swaps++;
         }
         System.out.print(", " + comps + ", " + (swaps / 3));
     }
     
+    // External-facing method for mergeSort()
     public static void mergeSort(int[] input) {
         mergeSort(0, input.length - 1, input);
         System.out.print(", " + compsM + ", " + (swapsM / 3));
     }
     
+    // Sorts the given array in ascending order using merge sort
     public static void mergeSort(int beg, int end, int[] input) {
-        
-        if (end == beg) {
+        if (beg == end) { // base case
             return;
-        }
-        else {
-            int mid = (end + beg) / 2;
+        } else {          // recursive case
+            int mid = (beg + end) / 2;
             mergeSort(beg, mid, input);
             mergeSort(mid + 1, end, input);
             merge(beg, mid, end, input);
         }
     }
     
+    // Merges the two partitions of the provided array
     public static void merge(int beg, int mid, int end, int input[]) {
         int[] temp = new int[(end - beg) + 1];
-        int firstPos = beg;
-        int secondPos = mid + 1;
+        int leftPos = beg;
+        int rightPos = mid + 1;
         int insertPos = 0;
+        
         compsM++;
-        while (firstPos <= mid && secondPos <= end) {
+        
+        // Runs through both partitions simultaneously.
+        while (leftPos <= mid && rightPos <= end) {
             compsM++;
             compsM++;
-            if (input[firstPos] < input[secondPos]) {
-                temp[insertPos] = input[firstPos];
+            
+            if (input[leftPos] < input[rightPos]) {
+                temp[insertPos] = input[leftPos];
                 swapsM++;
                 insertPos++;
-                firstPos++;
+                leftPos++;
             } else {
-                temp[insertPos] = input[secondPos];
+                temp[insertPos] = input[rightPos];
                 swapsM++;
                 insertPos++;
-                secondPos++;
+                rightPos++;
             }
         }
         compsM++;
-        while (firstPos <= mid) {
+        
+        // Finishes the 1st partition if the 2nd partition finished first.
+        while (leftPos <= mid) {
             compsM++;
             swapsM++;
-            temp[insertPos] = input[firstPos];
+            temp[insertPos] = input[leftPos];
             insertPos++;
-            firstPos++;
+            leftPos++;
         }
         compsM++;
-        while (secondPos <= end) {
+        
+        // Finishes the 2nd partition if the 1st partition finished first.
+        while (rightPos <= end) {
             compsM++;
             swapsM++;
-            temp[insertPos] = input[secondPos];
+            temp[insertPos] = input[rightPos];
             insertPos++;
-            secondPos++;
+            rightPos++;
         }
-        int tempIndex = 0;
-        for (int i = beg; i <= end; i++) {
-            input[i] = temp[tempIndex];
+        
+        for (int i = 0; i < temp.length; i++) {
+            input[i + beg] = temp[i];
             swapsM++;
-            tempIndex++;
         }
     }
 }
